0ec538d5bc2a76283611e78ef440d38076a89fa6
necdetcan <64875835+necdetcan@users.noreply.github.com>
2020-12-08 21:30:43 +0300

    Add files via upload

---
+++ Models/Pilotlar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebProje.Models
+{
+    public partial class Pilotlar
+    {
+        public int Id { get; set; }
+        public string Soyad { get; set; }
+        public string Ad { get; set; }
+        public int? Yas { get; set; }
+        public string Takim { get; set; }
+        public string Biyografi { get; set; }
+        public int? YarisGalibiyet { get; set; }
+        public int? PodyumSayi { get; set; }
+    }
+}
