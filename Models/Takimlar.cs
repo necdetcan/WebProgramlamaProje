2483ffde0d220fd441e50e144c712e63f1afde1c
necdetcan <64875835+necdetcan@users.noreply.github.com>
2020-12-28 21:42:51 +0300

    Add files via upload

---
+++ Models/Takimlar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebProje.Models
+{
+    public partial class Takimlar
+    {
+        public int Id { get; set; }
+        public string TakimAdi { get; set; }
+        public string TakimMerkezi { get; set; }
+        public string TakimPatronu { get; set; }
+        public string TeknikSef { get; set; }
+        public string Sasi { get; set; }
+        public string GucUnitesi { get; set; }
+        public string KurulusTarihi { get; set; }
+        public int? DunyaSampiyonluklari { get; set; }
+        public string EnIyiSonuc { get; set; }
+        public int? PolePozisyonu { get; set; }
+        public int? EnHizliTur { get; set; }
+    }
+}
