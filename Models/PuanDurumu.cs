ca38ebd280a0902e81f2a2d2dad997994be319ce
necdetcan <64875835+necdetcan@users.noreply.github.com>
2020-12-30 21:30:54 +0300

    Add files via upload

---
+++ Models/PuanDurumu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebProje.Models
+{
+    public partial class PuanDurumu
+    {
+        public int Id { get; set; }
+        public string Takim { get; set; }
+        public string Pilot { get; set; }
+        public int? Puan { get; set; }
+
+
+
+
+
+
+    }
+}
