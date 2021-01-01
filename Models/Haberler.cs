11cc5a129b6268bb462b4d0a8a75773e22e368a6
necdetcan <64875835+necdetcan@users.noreply.github.com>
2021-01-01 23:21:44 +0300

    Add files via upload

---
+++ Models/Haberler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebProje.Models
+{
+    public partial class Haberler
+    {
+        public int Id { get; set; }
+        public string HaberTr { get; set; }
+        public string HaberEng { get; set; }
+        public int? Begeni { get; set; }
+        public int? Dislike { get; set; }
+        public DateTime? Tarih { get; set; }
+        public string Baslik { get; set; }
+        public string BaslikEng { get; set; }
+    }
+}
