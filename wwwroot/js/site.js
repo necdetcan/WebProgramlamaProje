55fe7778db51bf91095ba2b827618ef1b05f33e1
necdetcan <64875835+necdetcan@users.noreply.github.com>
2021-01-03 03:09:10 +0300

    Add files via upload

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script>
+    const $ = s => document.querySelectorAll(s)
+    const on = (ev, el, cb) =>
+    el.addEventListener(ev, e => cb(e, el))
+    $('a').forEach(el =>
+    on('click', el, e => e.preventDefault())
+    )
+    </script>
